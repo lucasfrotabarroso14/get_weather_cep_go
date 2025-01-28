24bf9e75eb8667e38780fc430464ac0a9860eaa7
Lucas Barroso <lucas.barroso@pag.ai>
2025-01-28 17:06:25 -0300

    feat get location

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 type Input struct {
 	Cep string `json:"cep"`
 }
+type ViaCepResponse struct {
+	Cep        string `json:"cep"`
+	Logradouro string `json:"logradouro"`
+	Bairro     string `json:"bairro"`
+	Localidade string `json:"localidade"`
+	Uf         string `json:"uf"`
+}
 
 func main() {
 
@@ -15,13 +22,40 @@ func main() {
 
 	mux.HandleFunc("/busca", getWeatherHandler)
 
+	http.ListenAndServe(":8084", mux)
+
 }
 
 func getWeatherHandler(w http.ResponseWriter, r *http.Request) {
 
 	var inputDTO Input
-	if err := json.NewDecoder(r.Body).Decode(&inputDTO); err != nil {
+	err := json.NewDecoder(r.Body).Decode(&inputDTO)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	//var cep string
+
+	url_get_loc := "https://viacep.com.br/ws/" + inputDTO.Cep + "/json"
+
+	res, err := http.Get(url_get_loc)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer res.Body.Close()
+	var output ViaCepResponse
+
+	if err = json.NewDecoder(res.Body).Decode(&output); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	url_get_weather := "http://api.weatherapi.com/v1"
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(output)
+
 }
+http://api.weatherapi.com/v1
